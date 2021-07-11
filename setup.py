5d76af3d533de807636126361b8a292c7b42db0d
MWFK <mouafek.ayadi@esprit.tn>
2021-07-11 19:21:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scrap_Tunisian_Stock_Exchange',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/MWFK/Scrap_Tunisian_Stock_Market',
+    license='Apache License 2.0',
+    author='MXK',
+    author_email='mouafek.ayadi@esprit.tn',
+    description='Scrapper'
+)
